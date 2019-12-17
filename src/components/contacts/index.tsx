98c8390b5f3b9f30308deeb13edcd301e648856c
Firzinat Khuzeev <huzferd@gmail.com>
2019-12-18 01:47:24 +0300

    feat(contacs): add contacts component

--- src/components/contacts/index.tsx
+++ src/components/contacts/index.tsx
@@ -1,11 +1,53 @@
 import React from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import {
+    ContactsContainer,
+    AddressContainer,
+    FormContainer
+} from './styles';
 
 class Contacts extends React.Component<any, any> {
     render() {
         return (
-            <div>
-                <div>Contacts page</div>
-            </div>
+            <ContactsContainer>
+                <Map
+                    center={[55.7809, 49.1313]}
+                    zoom={15}
+                    maxZoom={18}
+                    attributionControl={true}
+                    zoomControl={true}
+                    doubleClickZoom={true}
+                    scrollWheelZoom={true}
+                    dragging={true}
+                    animate={true}
+                    easeLinearity={0.35}
+                >
+                    <TileLayer
+                        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={[55.7809, 49.1313]}>
+                        <Popup>
+                            Akvelon.
+                        </Popup>
+                    </Marker>
+                </Map>
+                <AddressContainer>
+                    <h2>Address</h2>
+                    <div>Peterburgskaya St, 50к23,<br />
+                        office 521,<br />
+                        Kazan, Tatarstan,<br />
+                        420107</div>
+                    <h2>Contact Us</h2>
+                    <FormContainer>
+                        <input type="text" placeholder="Name" />
+                        <input type="email" placeholder="Email" />
+                        <textarea id="story" name="story"
+                            rows={5} cols={33} placeholder="Message" />
+                        <button type="submit">Submit</button>
+                    </FormContainer>
+                </AddressContainer>
+            </ContactsContainer>
         );
     }
 }
---
+++ src/components/contacts/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ContactsContainer = styled.div`
+  display: flex;
+`;
+
+export const AddressContainer = styled.div`
+  margin: 15px;
+`;
+
+export const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
